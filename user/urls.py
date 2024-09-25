84fb004a441aa95029a3ad3c8249ad64dfb1a8a1
SH_kim <dal2018@naver.com>
2024-09-26 00:55:52 +0900

    초기셋팅 오류수정

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("signup/", views.UserCreateView.as_view),
+    path("signup/", views.UserCreateView.as_view()),
 
 ]
 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.generics import CreateAPIView
 
 
 class UserCreateView(APIView):
