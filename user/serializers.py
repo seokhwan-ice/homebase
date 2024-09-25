fe42d52698eecb3a1e714173e82af902c2ec01a7
SH_kim <dal2018@naver.com>
2024-09-26 03:13:00 +0900

    feat: 프로필 수정 구현
    -UserProfileView 의 put 요청일때 수정 구현
    -UpdateProfileSerializer 추가

--- user/serializers.py
+++ user/serializers.py
@@ -20,3 +20,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 # 추가적인 커뮤니티, 코멘트, 좋아요 구현후 필드추가.
+
+class UpdateProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email","phone_number", "nickname", "bio", "created_at"]
+
+# account- 내 계정(개인정보) 수정 페이지 추후 비번필드도 보여줌?
--- user/views.py
+++ user/views.py
@@ -7,15 +7,17 @@ from .models import User
 from .vaildators import validate_user_data
 from .serializers import UserSerializer, UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status
 
 class UserCreateView(APIView):
-    def post(self,request):
+    def post(self, request):
         rlt_message = validate_user_data(request.data)
         if rlt_message is not None:
-            return Response({"message": rlt_message},status=400)
-        
-        user = User.objects.create_user(**request.data) # 코드 간소화
-        
+            return Response({"message": rlt_message}, status=400)
+
+        user = User.objects.create_user(**request.data)  # 코드 간소화
+
         # user = User.objects.create_user(
         #     username=request.data.get("username"),
         #     nickname=request.data.get("nickname"),
@@ -28,31 +30,44 @@ class UserCreateView(APIView):
 
         serializer = UserSerializer(user)
         return Response(serializer.data)
-    
+
+
 class UserLoginView(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
-        
+
         user = authenticate(username=username, password=password)
         if not user:
-            return Response({"message": "아이디 또는 비밀번호가 틀렸습니다"}, status=400)
-        
+            return Response(
+                {"message": "아이디 또는 비밀번호가 틀렸습니다"}, status=400
+            )
 
         refresh = RefreshToken.for_user(user)
 
         return Response(
-        {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-        }
-    )
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+        )
+
 
 class UserProfileView(APIView):
-    permission_classes=[IsAuthenticated]
-    
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, username):
         user = get_object_or_404(User, username=username)
         serializer = UserProfileSerializer(user)
         return Response(serializer.data)
-    
+
+    def put(self, request, username):
+        user = get_object_or_404(User, username=username)
+        if request.user != user:
+            raise PermissionDenied("수정 권한이 없습니다")
+
+        serializer = UserProfileSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
