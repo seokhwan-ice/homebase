787b8ecec0198effef526f6d3e470e7eeb29e1c7
SH_kim <dal2018@naver.com>
2024-09-25 15:46:53 +0900

    초기셋팅

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=10, unique=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to="user/image/%Y/%m/%d/", null=True, blank=True)
+    phone_number = models.CharField(max_length=13, blank=True, validators=[RegexValidator(r"010-?[1-9]\d{3}-\d{4}")])
+
