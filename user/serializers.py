a7556a088143f248ebb9c945a35e77b623093ed5
SH_kim <dal2018@naver.com>
2024-10-02 11:00:09 +0900

    으으

--- user/serializers.py
+++ user/serializers.py
@@ -5,80 +5,80 @@ from community.models import Comment, Bookmark
 # TODO 모두 구현 간소화진행할게요! 코멘트도 하나의 시리얼라이져만들어서 사용하는거 고민!!
 
 
-# class DynamicFieldsModelSerializer(serializers.ModelSerializer):
-#     def __init__(self, *args, **kwargs):
-#         fields = kwargs.pop("fields", None)
-#         super().__init__(*args, **kwargs)
-#         # 파라쿼리미터로(뷰에서 불러올 필드) 제어
-#         if fields is not None:
-#             allowed = set(fields)  # 요청된
-#             existing = set(self.fields)
-#             for field_name in existing - allowed:
-#                 self.fields.pop(field_name)
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop("fields", None)
+        super().__init__(*args, **kwargs)
+        # 파라쿼리미터로(뷰에서 불러올 필드) 제어
+        if fields is not None:
+            allowed = set(fields)  # 요청된
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
 
-# #TODO 직관 이미지 추가해보고 실험 images.append(live_image.live_image)
-# class UserProfileSerializer(DynamicFieldsModelSerializer):
-#     following_count = serializers.SerializerMethodField()
-#     followers_count = serializers.SerializerMethodField()
-#     article_count = serializers.SerializerMethodField()
-#     comment_count = serializers.SerializerMethodField()
-#     bookmark_count = serializers.SerializerMethodField()
-#     community_free_title = serializers.SerializerMethodField()
-#     community_live_image = serializers.SerializerMethodField()
 
-#     def get_bookmark_count(self, obj):
-#         return Bookmark.objects.filter(user=obj).count()
+# TODO 직관 이미지 추가해보고 실험 images.append(live_image.live_image)
+class UserProfileSerializer(DynamicFieldsModelSerializer):
+    following_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    article_count = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
+    bookmark_count = serializers.SerializerMethodField()
+    community_free_title = serializers.SerializerMethodField()
+    community_live_image = serializers.SerializerMethodField()
 
-#     def get_comment_count(self, obj):
-#         return Comment.objects.filter(author=obj).count()
+    def get_bookmark_count(self, obj):
+        return Bookmark.objects.filter(user=obj).count()
 
-#     def get_article_count(self, obj):
-#         free_article = obj.author_free.count()
-#         live_article = obj.author_live.count()
-#         return free_article + live_article
+    def get_comment_count(self, obj):
+        return Comment.objects.filter(author=obj).count()
 
-#     def get_following_count(self, obj):
-#         return obj.followings.count()
+    def get_article_count(self, obj):
+        free_article = obj.author_free.count()
+        live_article = obj.author_live.count()
+        return free_article + live_article
 
-#     def get_followers_count(self, obj):
-#         return obj.followers.count()
-    
-#     def get_community_free_title(self, obj):
-#         user_free_article = obj.author_free.all()
-#         title = []
+    def get_following_count(self, obj):
+        return obj.followings.count()
 
-#         for free in user_free_article:
-#             if free.title:
-#                 title.append(free.title)
-#         return title
+    def get_followers_count(self, obj):
+        return obj.followers.count()
 
-#     def get_community_live_image(self, obj):
-#         user_live_image = obj.author_live.all()
-#         images = []
+    def get_community_free_title(self, obj):
+        user_free_article = obj.author_free.all()
+        title = []
 
-#         for live_image in user_live_image:
-#             if live_image.live_image:
-#                 images.append(live_image.live_image)
-#         return images
+        for free in user_free_article:
+            if free.title:
+                title.append(free.title)
+        return title
 
-#     class Meta:
-#         model = User
-#         fields = [
-#             "email",
-#             "phone_number",
-#             "profile_image",
-#             "nickname",
-#             "bio",
-#             "created_at",
-#             "following_count",
-#             "followers_count",
-#             "article_count",
-#             "comment_count",
-#             "bookmark_count",
-#             "community_free_title",
-#             "community_live_image",
-#         ]
+    def get_community_live_image(self, obj):
+        user_live_image = obj.author_live.all()
+        images = []
 
+        for live_image in user_live_image:
+            if live_image.live_image:
+                images.append(live_image.live_image)
+        return images
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "phone_number",
+            "profile_image",
+            "nickname",
+            "bio",
+            "created_at",
+            "following_count",
+            "followers_count",
+            "article_count",
+            "comment_count",
+            "bookmark_count",
+            "community_free_title",
+            "community_live_image",
+        ]
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -89,7 +89,6 @@ class UserSerializer(serializers.ModelSerializer):
             "nickname",
             "name",
         ]
-
 
 
 # class FollowingListSerializer(serializers.ModelSerializer):
--- user/views.py
+++ user/views.py
@@ -53,71 +53,32 @@ class UserViewSet(viewsets.ViewSet):
             }
         )
     
+    @action(detail=False, methods=["put"], permission_classes = [IsAuthenticated])
+    def password(self, request):
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
 
+        if not old_password or not new_password:
+            return Response({"message": " 비밀번호 똑디 입력해주세요"}, status=400)
 
+        if not request.user.check_password(old_password):
+            return Response({"message": "이전 비밀번호가 틀렸습니다."}, status=400)
 
-# class UserCreateView(APIView):
-#     def post(self, request):
-#         rlt_message = validate_user_data(request.data)
-#         if rlt_message is not None:
-#             return Response({"message": rlt_message}, status=400)
-#         user = User.objects.create_user(**request.data)  # 코드 간소화
-#         serializer = UserSerializer(user)
-#         return Response(serializer.data)
+        request.user.set_password(new_password)
+        request.user.save()
 
+        # 새로운 토큰 발급
+        refresh = RefreshToken.for_user(request.user)
+        access_token = refresh.access_token
 
-# class UserLoginView(APIView):
-#     def post(self, request):
-#         username = request.data.get("username")
-#         password = request.data.get("password")
-
-#         user = authenticate(username=username, password=password)
-#         if not user:
-#             return Response(
-#                 {"message": "아이디 또는 비밀번호가 틀렸습니다"}, status=400
-#             )
-
-#         refresh = RefreshToken.for_user(user)
-
-#         return Response(
-#             {
-#                 "refresh": str(refresh),
-#                 "access": str(refresh.access_token),
-#             }
-#         )
-
-
-# class UserPasswordChangeView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def put(self, request):
-
-#         old_password = request.data.get("old_password")
-#         new_password = request.data.get("new_password")
-
-#         if not old_password or not new_password:
-#             return Response({"message": " 비밀번호 똑디 입력해주세요"}, status=400)
-
-#         if not request.user.check_password(old_password):
-#             return Response({"message": "이전 비밀번호가 틀렸습니다."}, status=400)
-
-#         request.user.set_password(new_password)
-#         request.user.save()
-
-#         # 새로운 토큰 발급
-#         refresh = RefreshToken.for_user(request.user)
-#         access_token = refresh.access_token
-
-#         return Response(
-#             {
-#                 "message": "비밀번호 변경 성공!",
-#                 "access": str(access_token),
-#                 "refresh": str(refresh),
-#             },
-#             status=200,
-#         )
-
-
+        return Response(
+            {
+                "message": "비밀번호 변경 성공!",
+                "access": str(access_token),
+                "refresh": str(refresh),
+            },
+            status=200,
+        )
 # class UserDeleteView(APIView):
 #     permission_classes = [IsAuthenticated]
 
